8ff7a9368cfdb874feabf9a24e6c4814cffab513
amolmate <mate.amol18@gmail.com>
2020-04-15 00:37:04 +0530

    log added

--- src/main/java/com/imageupload/ApplicationImageUpload.java
+++ src/main/java/com/imageupload/ApplicationImageUpload.java
@@ -1,5 +1,8 @@
 package com.imageupload;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,6 @@ public class ApplicationImageUpload {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationImageUpload.class, args);
-		System.out.println("Hello Spring Image Uplaod");
+		Logger.getLogger("").log(Level.ALL, "Hi in main");
 	}
 }
